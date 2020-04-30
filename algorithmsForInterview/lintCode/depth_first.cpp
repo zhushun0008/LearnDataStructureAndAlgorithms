7042d93ea28d7769017bbe445a462574f697c675
zhushun03 <zhushun03@kuaishou.com>
2020-04-30 15:30:29 +0800

    add .gitignore file

---
+++ algorithmsForInterview/lintCode/depth_first.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zhu Kevin on 2020/4/25.
+//
+
+#include "depth_first.h"
---
+++ algorithmsForInterview/lintCode/depth_first.h
@@ -0,0 +1,14 @@
+//
+// Created by Zhu Kevin on 2020/4/25.
+//
+
+#ifndef LINTCODE_DEPTH_FIRST_H
+#define LINTCODE_DEPTH_FIRST_H
+
+
+class depth_first {
+
+};
+
+
+#endif //LINTCODE_DEPTH_FIRST_H
